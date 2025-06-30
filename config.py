f6163586827557bacfc30837ffdf75261fc1cd1d
trap1boy <mahdiafkoni@gmail.com>
2025-06-30 04:40:18 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+BOT_TOKEN = "7705956184:AAGzgWtufVUiQhod1UxYGdEqBTl5kzy7TOQ"
+FORCE_CHANNEL = "@ixtrap"  # آیدی کانال اجباری بدون فاصله و بدون https
+ADMIN_ID = 7829262788  # آیدی عددی ادمین برای دریافت فیدبک و پیام‌ها
